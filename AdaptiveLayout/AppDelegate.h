d69c857e49c8c7861343be185d37a7d0d3694a61
Miguel Uribe <migueluribe@Miguels-Mac.local>
2015-05-29 16:50:08 -0700

    Initial Commit

---
+++ AdaptiveLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdaptiveLayout
+//
+//  Created by Miguel Uribe on 29/05/15.
+//  Copyright (c) 2015 Miguel Uribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdaptiveLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptiveLayout
+//
+//  Created by Miguel Uribe on 29/05/15.
+//  Copyright (c) 2015 Miguel Uribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
